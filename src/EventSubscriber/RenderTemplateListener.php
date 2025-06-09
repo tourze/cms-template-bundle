4726f747c84d8b2001bfeb47fbee110c79154f68
github-ci <you@example.com>
2025-06-09 15:02:46 +0800

    新增 `cms-template-bundle` 包，实现基础功能，包括 `AdminMenu` 服务、模板渲染实体、依赖注入扩展等，完善单元测试和文档，确保整体功能完整性与测试覆盖率。

---
+++ src/EventSubscriber/RenderTemplateListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tourze\CmsTemplateBundle\EventSubscriber;
+
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\Events;
+use Tourze\CmsTemplateBundle\Entity\RenderTemplate;
+
+#[AsEntityListener(event: Events::prePersist, method: 'prePersist', entity: RenderTemplate::class)]
+#[AsEntityListener(event: Events::preUpdate, method: 'preUpdate', entity: RenderTemplate::class)]
+class RenderTemplateListener
+{
+    public function prePersist(RenderTemplate $object): void
+    {
+        $this->ensurePathHasLeftSlash($object);
+    }
+
+    public function preUpdate(RenderTemplate $object): void
+    {
+        $this->ensurePathHasLeftSlash($object);
+    }
+
+    public function ensurePathHasLeftSlash(RenderTemplate $object): void
+    {
+        $path = trim($object->getPath());
+        if (!str_starts_with($path, '/')) {
+            $path = "/{$path}";
+        }
+
+        $object->setPath($path);
+    }
+}
